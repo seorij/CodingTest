fd657348c1abfa6fec93af6ffa7071e0c36cbcd6
seorij <jseolah06@gmail.com>
2022-04-10 12:46:43 +0900

    프로그래머스 k번째수

--- src/Programmers/sort1.java
+++ src/Programmers/sort1.java
@@ -1,9 +1,19 @@
 //K번째수
 package Programmers;
-import java.util.Scanner;
+import java.util.*;
 
 public class sort1 {
-    public static void main(String[] args) {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
 
+        for(int i = 0; i < commands.length; i++) {
+            int[] temp = new int[commands[i][1]-(commands[i][0]-1)];
+            for(int j = 0; j < temp.length; j++) {
+                temp[j] = array[j+commands[i][0]-1]; //이게 왜 여기서 이러는지
+            }
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+        return answer;
     }
 }
