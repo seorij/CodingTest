ca7a847bf1af61455880bba68c367919ebe61503
seorij <jseolah06@gmail.com>
2022-04-13 11:58:56 +0900

    코드업 1차원 배열

--- src/CodeUp/Array/C1420.java
+++ src/CodeUp/Array/C1420.java
@@ -1,8 +1,17 @@
 package CodeUp.Array;
-import java.util.Scanner;
+import java.util.*;
 
 public class C1420 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        char[][] a = new char[n][2];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 2; j++) {
+                a[i][j] = sc.next().charAt(i);
+            }
+        }
     }
 }
